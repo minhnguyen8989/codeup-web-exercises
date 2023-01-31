bdae5b0c759f2ec20bf1370b8d9d67596ada4420
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-01-31 10:53:08 -0600

    Completed exercises with iterating array

--- JS/iterating.js
+++ JS/iterating.js
@@ -6,29 +6,49 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    console.log("-----------------------")
+    var names = ["mike", "tony", "minh", "victor"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    console.log("-----------------------")
+    console.log(names.length);
+
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log("-----------------------")
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    console.log("-----------------------")
+    for(var i = 0; i < names.length; i++){
+        console.log("The index number: " + i + " in names' array is: " + names[i].toUpperCase());
+    }
+
+
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    console.log("-----------------------")
+    names.forEach(function (name,index,array){
+        console.log("The index number: " + index + " in names array is: " + name);
+    })
 
     /**
      * TODO:
@@ -43,5 +63,21 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    console.log("-----------------------")
+    function first(array){
+        return console.log(array[0]);
+    }
+    first([1, 2, 3, 4, 5])
+
+    function second(array){
+        return console.log(array[1]);
+    }
+    second([1, 2, 3, 4, 5])
+
+
+    function last(array){
+        return console.log(array[array.length - 1]);
+    }
+    last([1, 2, 3, 4, 5])
 
 })();
