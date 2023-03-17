84203a78a6e0ab92b7445822b8d014abf5397608
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-17 10:36:58 -0500

    continue on witht exercise

--- JS/es6.js
+++ JS/es6.js
@@ -47,19 +47,17 @@ users.push({
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-    return emails.push(user.email);
+users.forEach((user) => {emails.push(user.email);
 });
-users.forEach(function(user) {
-    return names.push(user.name);
+users.forEach((user) => {names.push(user.name);
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
