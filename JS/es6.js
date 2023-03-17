43a8eebd87885827190da7c6d2a872443b2f0706
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-17 10:39:43 -0500

    continue on with exercise

--- JS/es6.js
+++ JS/es6.js
@@ -62,19 +62,19 @@ users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
-    const name = user.name;
-    const email = user.email;
-    const languages = user.languages;
+    name;
+    email;
+    languages;
 
     // TODO: rewrite the assignment below to use template strings
     developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+(developer of developers) {
 
     // TODO: rewrite the assignment below to use template strings
     list += '<li>' + developer + '</li>';
