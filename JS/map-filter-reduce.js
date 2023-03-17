ea5261bd029cb36da2d8528b44997e8d14e5df41
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-17 15:23:05 -0500

    complete exercise with bonus

--- JS/map-filter-reduce.js
+++ JS/map-filter-reduce.js
@@ -80,3 +80,19 @@ let stringNames = users.reduce(function (allNames, names){
     return allNames + names.name + ", ";
 },"")
 console.log(stringNames);
+
+
+
+console.log("-----------------");
+//Use .reduce to get the unique list of languages from the list of users.
+
+let getUsersLanguage = users.reduce(function (totalLanguage, language){
+    return totalLanguage + language.languages
+},"")
+let splitLanguage = getUsersLanguage.trim().split(',');
+
+let uniqueLanguage = splitLanguage.filter(function (item, i, allItems){
+    return i==allItems.indexOf(item);
+})
+
+console.log(uniqueLanguage);
